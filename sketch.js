a2458ac1727386f8c3dd5db18020d90a70a0c3f3
Megha Nayar <96401366+nayarmegha@users.noreply.github.com>
2022-09-28 17:12:47 -0500

    Update sketch.js
    updated colors of the sketch

--- sketch.js
+++ sketch.js
@@ -1,10 +1,10 @@
 
 let clrs = [
-'#E27F34', //top 0
-'#FEF254', //middle top 1
-'teal', //middle strip 2
-'#042060', //middle bottom 3
-'#3778AE' //bottom 4
+'yellow', //top 0
+'violet', //middle top 1
+'olive', //middle strip 2
+'lime', //middle bottom 3
+'green' //bottom 4
 ]
 
 
